e3dc01f037dd3da1f7654c6b68eeb631b837d64a
spapicchio <simopapi@hotmail.com>
2023-05-19 09:31:07 +0000

    feat: add tag metrics
    * add abstract_metric.py abstract_method evaluate_single_no_special_case
    * add cell_precision_tag.py
    * add cell_recall_tag.py
    * add row_cardinality_tag.py
    * add tuple_cardinality_tag.py
    * add tuple_order_tag.py

---
+++ src/metrics/cell_precision_tag.py
@@ -0,0 +1,24 @@
+import itertools
+
+from .abstract_metric import AbstractMetric
+
+
+class CellPrecisionTag(AbstractMetric):
+    def evaluate_single_no_special_case(self, target: list[list],
+                                        prediction: list[list]) -> float:
+        """
+        the ratio of predicted cell that are in the target.
+        Does not consider cardinality (measured by other tags)
+        • High PRECISION means that the model is good at
+          identifying relevant instances and has a low false positive rate.
+
+        :param target: target table to be compared with prediction table
+        :param prediction: prediction table to be compared with target table
+        :return: score between [0,1]
+            * 0 indicates no cell in the prediction is in the target
+            * 1 indicates all cells in the prediction are in the target
+        """
+        flat_target = list(itertools.chain(*target))
+        flat_prediction = list(itertools.chain(*prediction))
+        sum_cell_match = sum([1 for cell in flat_prediction if cell in flat_target])
+        return round(sum_cell_match / len(flat_prediction), 3)
