cbec707da379ddbaf26e8b2ecb038d8bbc4b1ebe
spapicchio <simopapi@hotmail.com>
2023-09-15 14:38:27 +0000

    feat: add simpler way to run QATCH components
    * feat add 01_generate_tests.py to generate tests for both proprietary and spider tables
    * feat add 02_predicts_tests.py to predict for Tapas, Tapex, Omnitab, ChatGPT-QA and ChatGPT-SP
    * feat add 03_evaluate_tests.py to evaluate the predictions in both cases SP and QA

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,6 @@
 from .chatgpt import ChatGPT
+from .omnitab import Omnitab
 from .tapas import Tapas
 from .tapex import Tapex
 
-__all__ = ['Tapex', 'Tapas', 'ChatGPT']
+__all__ = ['Tapex', 'Tapas', 'ChatGPT', 'Omnitab']
