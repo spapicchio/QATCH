91ec062227df2d4c69e070cac594c57923b70a91
spapicchio <simopapi@hotmail.com>
2023-10-28 11:07:48 +0000

    feat: add single database wrapper for qatch package
    * add SingleDatabase class
        * add manage database already exist
        * add creation of the database with tables as input
        * !remove table stored in memory
        * !add table schema stored in memory with table name
        *
    * add test_single_database.py

---
+++ qatch/database_reader/single_database.py
@@ -0,0 +1,101 @@
+import logging
+import os.path
+import shutil
+import sqlite3
+
+import pandas as pd
+
+
+class SingleDatabase:
+    """Provides a simplified interface for interacting with SQLite databases in Python.
+
+    :ivar sqlite3.Connection conn: A connection object representing the SQLite database.
+    :ivar sqlite3.Cursor cursor: A cursor object used to execute SQL commands and retrieve results.
+    :ivar str db_name: The name of the database.
+    :ivar dict table_schemas: table name as key, table schema as value
+        table_schema is pd.DataFrame with columns: cid, name, type, notnull, dflt_value, pk
+    :ivar dict table_names: the names of the tables in the database
+    """
+
+    def __init__(self, db_path: str, db_name: str, tables: dict[str, pd.DataFrame] | None = None):
+        """
+        :param str db_path: The path where the database file will be stored, or it is already stored.
+        :param str db_name: The name of the database file (without the file extension).
+        :param dict tables: A dictionary containing table names as keys and corresponding Pandas DataFrames as values.
+                           If provided, these tables will be created in the database upon initialization.
+                           Default is None.
+
+        :raises ValueError: If the specified `db_path` does not exist and no tables are provided.
+        """
+        db_path = os.path.join(db_path, db_name)
+        if not os.path.exists(db_path):
+            os.makedirs(db_path)
+        path_sqlite_file = os.path.join(db_path, f'{db_name}.sqlite')
+        # This creates a connection object that represents the database in case it does not exist
+        conn = sqlite3.connect(path_sqlite_file)
+        conn.text_factory = lambda b: b.decode(errors='ignore')
+        # A cursor is used to execute SQL commands and retrieve results.
+        cursor = conn.cursor()
+        # select all the table names from the database if any
+        cursor.execute('SELECT name from sqlite_master where type= "table"')
+        existing_tables = [tbl[0] for tbl in cursor.fetchall()]
+        if len(existing_tables) == 0:
+            if tables is None:
+                # CASE 1: no tables provided and database does not exist
+                # remove the created file
+                conn.close()
+                shutil.rmtree(db_path.split(db_name)[0])
+                raise ValueError(f"Database path does not exist and no tables were provided "
+                                 f"to create the new database in the given path:"
+                                 f" {path_sqlite_file}")
+            else:
+                # CASE 2: tables provided and database does not exist
+                logging.info(f"provided tables are stored in {path_sqlite_file}")
+                existing_tables = list(tables.keys())
+                self._set_tables_in_db(tables, conn)
+        else:
+            if tables is None:
+                # CASE 3: database already exists and tables are provided
+                # log warning tables provided but also database already exists
+                logging.warning("tables provided but also database already exists, "
+                                "tables in the database will be used")
+        self.db_path = path_sqlite_file
+        self.conn = conn
+        self.cursor = cursor
+        self.table_names = existing_tables
+        self.table_schemas = {tbl_name: pd.read_sql_query(f"PRAGMA table_info({tbl_name})", self.conn)
+                              for tbl_name in self.table_names}
+
+    @staticmethod
+    def _set_tables_in_db(tables: dict[str, pd.DataFrame] | None, conn: sqlite3.Connection):
+        """
+        Sets the tables in the database based on the provided dictionary.
+        :param dict tables: A dictionary containing table names as keys and corresponding Pandas DataFrames as values.
+        :param sqlite3.Connection conn: A connection object representing the SQLite database.
+        """
+        for name, table in tables.items():
+            if name == 'table':
+                name = 'my_table'
+            table.to_sql(name, conn, if_exists='replace', index=False)
+
+    def get_table_from_name(self, table_name: str) -> pd.DataFrame:
+        """
+        Retrieves a specified table from the database as a Pandas DataFrame.
+        :param str table_name: The name of the table to retrieve from the database.
+        :return: A Pandas DataFrame representing the specified table from the database.
+        """
+        return pd.read_sql_query(f"SELECT * FROM {table_name}", self.conn)
+
+    def get_schema_given(self, table_name: str) -> pd.DataFrame:
+        """given the table name, returns the schema of the table
+        table_schema is pd.DataFrame with columns: cid, name, type, notnull, dflt_value, pk"""
+        return self.table_schemas[table_name]
+
+    def run_query(self, query: str) -> list[list]:
+        """Run a query on the database and return the result
+        only the first _MAX_TABLE_SIZE rows are analyzed"""
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+    def close_connection(self):
+        self.conn.close()
---
+++ tests/test_single_database.py
@@ -0,0 +1,87 @@
+import os
+import shutil
+import sqlite3
+
+import pandas as pd
+import pytest
+
+from qatch.database_reader import SingleDatabase
+
+# Define test data
+DB_PATH = 'test_db'
+DB_NAME = 'test_database'
+TABLE_NAME = 'test_table'
+TABLE_SCHEMA = {'id': [1, 2, 3], 'name': ['Alice', 'Bob', 'Charlie']}
+TABLE_DATAFRAME = pd.DataFrame(TABLE_SCHEMA)
+
+
+# Fixture to create a temporary SingleDatabase object for testing
+@pytest.fixture
+def single_database():
+    # Setup: Create a temporary database and table for testing
+    db = SingleDatabase(db_path=DB_PATH, db_name=DB_NAME, tables={TABLE_NAME: TABLE_DATAFRAME})
+    yield db  # Provide the fixture object
+    # Teardown: Clean up the temporary database and tables after testing
+    db.close_connection()
+    shutil.rmtree(DB_PATH)
+
+
+# Tests for SingleDatabase class methods
+def test_get_table_from_name(single_database):
+    table_data = single_database.get_table_from_name(TABLE_NAME)
+    assert table_data.equals(TABLE_DATAFRAME)
+
+
+def test_get_schema_given(single_database):
+    schema = single_database.get_schema_given(TABLE_NAME)
+    expected_schema = pd.read_sql_query(f"PRAGMA table_info({TABLE_NAME})", single_database.conn)
+    assert schema.equals(expected_schema)
+
+
+def test_run_query(single_database):
+    query = f"SELECT * FROM {TABLE_NAME}"
+    result = single_database.run_query(query)
+    expected_result = [(1, 'Alice'), (2, 'Bob'), (3, 'Charlie')]
+    assert result == expected_result
+
+
+def test_nonexistent_table(single_database):
+    with pytest.raises(pd.errors.DatabaseError):
+        single_database.get_table_from_name('nonexistent_table')
+
+
+def test_nonexistent_schema(single_database):
+    with pytest.raises(KeyError):
+        single_database.get_schema_given('nonexistent_table')
+
+
+def test_existing_db_without_tables():
+    # Setup: Create an empty database without providing tables
+    db_path = os.path.join(DB_PATH, DB_NAME)
+    if not os.path.exists(db_path):
+        os.makedirs(db_path)
+        conn = sqlite3.connect(os.path.join(db_path, f'{DB_NAME}.sqlite'))
+        conn.close()
+
+    # Test: Attempt to initialize SingleDatabase without tables
+    with pytest.raises(ValueError):
+        SingleDatabase(db_path=DB_PATH, db_name=DB_NAME)
+
+
+def test_existing_db_with_tables():
+    # Setup: Create a database with existing tables
+    db_path = os.path.join(DB_PATH, DB_NAME)
+    if not os.path.exists(db_path):
+        os.makedirs(db_path)
+        conn = sqlite3.connect(os.path.join(DB_PATH, DB_NAME, f'{DB_NAME}.sqlite'))
+        cursor = conn.cursor()
+        cursor.execute(f"CREATE TABLE {TABLE_NAME} (id INTEGER, name TEXT)")
+        conn.commit()
+        conn.close()
+
+    # Test: Initialize SingleDatabase with tables
+    db = SingleDatabase(db_path=DB_PATH, db_name=DB_NAME, tables={TABLE_NAME: TABLE_DATAFRAME})
+    assert TABLE_NAME in db.table_names
+    # Teardown: Close the connection and remove the temporary database directory
+    db.close_connection()
+    shutil.rmtree(DB_PATH)
