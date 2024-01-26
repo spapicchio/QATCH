fe514926cb3871b88c7bc3e0ede9313b0a482402
spapicchio <simopapi@hotmail.com>
2024-01-26 17:06:12 +0000

    Update metric evaluator and corresponding tests for clarity
    * The MetricEvaluator class in qatch/metric_evaluator.py has been significantly updated to improve clarity and efficiency
    * feat add `evaluate_single_test_QA` and `evaluate_single_test_SP` to evaluate metrics for a single test of QA and SP tasks respectively
    * rename function _create_mask_target_equal_prediction` to `are_cleaned_sql_identical` to better reflect its purpose
    * update tests/test_metric_evaluator.py reflecting the modifications in the MetricEvaluator class

--- qatch/metric_evaluator.py
+++ qatch/metric_evaluator.py
@@ -1,6 +1,7 @@
 import logging
 import sqlite3
 
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
@@ -42,106 +43,146 @@ class MetricEvaluator:
         self.metrics = metrics if isinstance(metrics, list) else [metrics]
 
         self._tags_generator = {
-            'cell_precision': CellPrecisionTag,
-            'cell_recall': CellRecallTag,
-            'tuple_cardinality': TupleCardinalityTag,
-            'tuple_constraint': TupleConstraintTag,
-            'tuple_order': TupleOrderTag,
+            'cell_precision': CellPrecisionTag(),
+            'cell_recall': CellRecallTag(),
+            'tuple_cardinality': TupleCardinalityTag(),
+            'tuple_constraint': TupleConstraintTag(),
+            'tuple_order': TupleOrderTag(),
         }
         self.databases = databases
 
-    def evaluate_with_df(self, df, prediction_col_name: str, task: str, keep_target: bool = False
+    def evaluate_with_df(self, df, prediction_col_name: str, task: str, target_col_name: str = 'query',
+                         keep_target: bool = False
                          ) -> pd.DataFrame:
-        """
-        Evaluate the specified metrics using the provided DataFrame containing query results and predictions.
-        The df must contain 'query' and 'db_id' columns, and the "prediction_col_name" must be present in the df.
-        For SP task, it generates the prediction results for the queries that are not equal to the target.
+        """Evaluates SQL queries for various metrics including cell precision, cell recall,
+        tuple cardinality, tuple constraint, tuple order.
+
+        For each row in the input DataFrame, it evaluates either the task as a QA
+        (Question Answering) or SP (Semantic Parsing). Then, it concatenates the original DataFrame
+        and the evaluated metric DataFrame.
 
         Args:
-            df (pd.DataFrame): The DataFrame containing query results, prediction, and other relevant columns.
-            prediction_col_name (str): The name of the column containing prediction values.
-            task (str): The task type, either 'SP' for SQL prediction or other task types.
-            keep_target (bool): Whether to keep the target column in the output DataFrame. Default is False.
+            df (pd.DataFrame): Input DataFrame where each row represents a test.
+            prediction_col_name (str): Name of the column in the DataFrame that contains predictions.
+            task (str): Type of evaluation task. Could be `QA` or `SP`.
+            target_col_name (str): Name of the column in the DataFrame that contains target queries.
+            Default is `'query'`.
+            keep_target (bool): FALSE by default. If TRUE, keeps the target query.
 
         Returns:
-            pd.DataFrame: A DataFrame with added metric columns.
-
-        Examples:
-            Given a DataFrame "tests_df" with the following columns: ['query', 'db_id', 'pred_tapas']
-            and a MultipleDatabases object connected with the db_id,
-            >>> evaluator = MetricEvaluator(databases)
-            >>> tests_df = evaluator.evaluate_with_df(tests_df, 'pred_tapas', 'SP')
-            >>> tests_df.columns.tolist()
-            [query, db_id, pred_tapas, cell_precision_pred_tapas, cell_recall_pred_tapas, tuple_cardinality_pred_tapas, tuple_constraint_pred_tapas, tuple_order_pred_tapas]
+            pd.DataFrame: Output DataFrame that has the original DataFrame along with the evaluated metric DataFrame.
         """
-        # get target values
-        df, target_col_name = self._get_query_results_from_db(df)
-        if task == 'SP':
-            # get prediction values for the SP task and the new prediction_col_name
-            df, prediction_col_name = self._get_SP_query_results_from_db(df, prediction_col_name)
+        if task.upper() == 'QA':
+            # add the new metrics at the bottom of the dataframe
+            df_metrics = df.apply(lambda row: self.evaluate_single_test_QA(row,
+                                                                           prediction_col_name,
+                                                                           target_col_name),
+                                  axis=1, result_type='expand')
+        else:
+            df_metrics = df.apply(lambda row: self.evaluate_single_test_SP(row,
+                                                                           prediction_col_name,
+                                                                           target_col_name), axis=1,
+                                  result_type='expand')
+        return pd.concat([df, df_metrics], axis=1).replace({np.nan: None})
 
-        # only the test where order is present
-        queries = df['query'].str.lower()
-        mask_order = queries.str.contains('order')
+    def evaluate_single_test_QA(self, test: dict | pd.Series, prediction_col_name: str, target_col_name: str) -> dict:
+        """
+        Evaluates metric scores on a single test QA task where a test is a dictionary (or pd.Series) and the
+        `prediction_col_name` and `target_col_name` are the column names in the test data containing model predictions
+        and actual target values respectively.
 
-        # create the mask for the EQUAL case (no need to run the evaluation)
-        mask_equal = df[prediction_col_name] == 'EQUAL'
+        Args:
+            test (dict | pd.Series): A dictionary or pandas Series containing a single test data. The keys (columns for Series)
+                should include `prediction_col_name` and `target_col_name`.
+            prediction_col_name (str): String representing the key in `test` dictionary (or column in `test` pandas Series)
+                where the predicted values are.
+            target_col_name (str): String representing the key in `test` dictionary (or column in `test` pandas Series)
+                where the actual target values are.
 
+        Returns:
+            dict: A dictionary with keys are metric name and value is the evaluated metric score for each metric in `self.metrics`.
+
+        Example:
+            >>> eval_task = MetricEvaluator(databases, metrics=['cell_precision', 'cell_recall'])
+            >>> test = {"sql_tags": "SELECT",
+            ...         "prediction": [["wales", "scotland"], ["england"]],
+            ...         "target": [["scotland", "wales"], ["england"]]}
+            >>> prediction_col_name = "prediction"
+            >>> target_col_name = "target"
+            >>>result = eval_task.evaluate_single_test_QA(test, prediction_col_name, target_col_name)
+            >>> print(result)
+            {'cell_precision_prediction': 0.66, 'cell_recall_prediction': 1.0}
+
+        Notes:
+            In the above example, `cell_precision_prediction` is calculated as 2/3 = 0.66,
+            "wales" and "scotland" are in the correct position.
+            `cell_recall_prediction` is 2/2 = 1, all actual outcomes are included in the prediction.
+        """
+
+        # Runs the target query on the database
+        new_target_col = f'{target_col_name}_result'
+        test[new_target_col] = self.databases.run_query(test['db_id'], test[target_col_name])
+
+        metric2evaluation = {f'{metric}_{prediction_col_name}': None for metric in self.metrics}
         for metric in self.metrics:
-            generator = self._tags_generator[metric]()
+            generator = self._tags_generator[metric]
             # initialize the metric column
-            metric_col_name = f'{metric}_{prediction_col_name}'
-            df.loc[:, metric_col_name] = None
-            if metric == 'tuple_order':
-                # when the target and prediction are equal, the metric is 1
-                df.loc[mask_order & mask_equal, metric_col_name] = 1
-                mask = mask_order & ~mask_equal
-            else:
-                # when the target and prediction are equal, the metric is 1
-                df.loc[mask_equal, metric_col_name] = 1
-                mask = ~mask_equal
-
             # evaluate the metric only for the test where the prediction is not equal to the target
-            tqdm.pandas(desc=f'Evaluating {metric_col_name}')
-            df.loc[mask, metric_col_name] = df[mask].progress_apply(
-                lambda r: generator.evaluate_single_test_metric(r[target_col_name], r[prediction_col_name]),
-                axis=1)
-        # at the end drop the columns that are not needed anymore
-        if not keep_target:
-            df = df.drop(columns=[target_col_name, prediction_col_name]) if task == 'SP' \
-                else df.drop(columns=[target_col_name])
-        return df
+            tqdm.pandas(desc=f'Evaluating {metric}')
+            if metric == 'tuple_order' and 'order' not in test[target_col_name].lower():
+                continue
+            evaluation = generator.evaluate_single_test_metric(test[new_target_col], test[prediction_col_name])
+            metric2evaluation[f'{metric}_{prediction_col_name}'] = evaluation
+        return metric2evaluation
 
-    def _get_query_results_from_db(self, df) -> tuple[pd.DataFrame, str]:
+    def evaluate_single_test_SP(self, test: dict | pd.Series, prediction_col_name: str, target_col_name: str) -> dict:
         """
-        Retrieve query results for the "query" column.
-        Since these queries represent the target values, this function raises an error if the query is not valid.
+        Evaluates metrics for a single SQL prediction test by fetching the results of the predicted and
+        target queries from the database.
+
+        This function fetches results based on provided `prediction_col_name` and `target_col_name`. Then it evaluates
+        performance of the prediction by invoking `evaluate_single_test_QA`.
 
         Args:
-            df (pd.DataFrame): The input DataFrame containing query information.
+            self (MetricEvaluator): The object instance the method is called on.
+            test (dict | pd.Series): The test data as a dictionary or pandas Series. It contains the 'db_id' (database identifier).
+                                     It is expected to have 'predictions_SP' and 'target_SP' keys/columns updated in process.
+            prediction_col_name (str): The name of column where prediction is stored.
+            target_col_name (str): The name of column where the target is stored.
 
         Returns:
-            tuple[pd.DataFrame, str]: A tuple containing the new DataFrame with query results and the column name for query results.
+            dict: A dictionary containing evaluation results obtained from `evaluate_single_test_QA`.
 
-        Raises:
-            sqlite3.OperationalError: If the query is not valid (the target must be correct).
+        Notes:
+            If the predicted query cannot be run on the db, the resulting metrics are all zeros
+
+        Example:
+            >>> test = {'db_id': 'database1', 'target': 'SELECT DISTINCT emailisfree FROM fraud', 'prediction': 'SELECT emailsisfree, income FROM fraud'}
+            >>> evaluator = MetricEvaluator(databases)
+            >>> results = evaluator.evaluate_single_test_SP(test, 'prediction', 'target')
+            >>> print(results)
+            {'cell_precision_prediction': 0.50, 'cell_recall_prediction': 1.0}
         """
-        query_column = 'query'
-        # group-by the df for each db_id present
-        grouped_by_db_df = df.groupby('db_id').agg(list)
-        # for each db_id get the results of the query from the db
-        tqdm.pandas(desc='Getting target results')
-        grouped_by_db_df[f'{query_column}_result'] = grouped_by_db_df.progress_apply(
-            lambda row: self.databases.run_multiple_queries(row.name, row[query_column]),
-            axis=1
-        )
-        # expand the grouped df
-        columns = grouped_by_db_df.columns.tolist()
-        df = grouped_by_db_df.explode(columns).reset_index()
-        return df, f'{query_column}_result'
+        # Compares the target and predicted SQL queries after cleaning and formatting. If they are identical, it returns metrics as 1
+        if self.are_cleaned_sql_identical(test[target_col_name], test[prediction_col_name]):
+            metrics_result = {f'{metric}_{prediction_col_name}': 1 for metric in self.metrics}
+            metrics_result[f'tuple_order_{prediction_col_name}'] = None \
+                if 'order' not in test[target_col_name].lower() else 1
+            return metrics_result
+
+        # Tries to run the predicted query on the database. If there is an error (e.g. syntax error in the query),
+        # it logs the error and returns metrics as 0
+        try:
+            test[prediction_col_name] = self.databases.run_query(test['db_id'], test[prediction_col_name])
+        except sqlite3.Error as e:
+            # catch any possible error of prediction and return all zeros
+            logging.error(e)
+            return {f'{metric}_{prediction_col_name}': 0 for metric in self.metrics}
+        # Evaluates the results of the target and predicted queries using the evaluate_single_test_QA function
+        return self.evaluate_single_test_QA(test, prediction_col_name, target_col_name)
 
     @staticmethod
-    def _create_mask_target_equal_prediction(target: str, prediction: str) -> bool:
+    def are_cleaned_sql_identical(target: str, prediction: str) -> bool:
         """
         Create a mask based on whether the target and prediction strings are equal after cleaning.
 
@@ -153,54 +194,18 @@ class MetricEvaluator:
             bool: True if cleaned prediction equals cleaned target, False otherwise.
         """
         new_target = (target.lower()
-                      .replace(" ,", ",").replace("  ", " ").replace('"', '').replace("'", '')
+                      .replace(" ,", ",")
+                      .replace("  ", " ")
+                      .replace('"', '')
+                      .replace("'", '')
                       .strip())
 
         new_pred = (prediction.lower()
-                    .replace(" ,", ",").replace("  ", " ").replace('"', '').replace("'", '')
-                    .replace(' ( ', '(').replace(' )', ')')
+                    .replace(" ,", ",")
+                    .replace("  ", " ")
+                    .replace('"', '')
+                    .replace("'", '')
+                    .replace(' ( ', '(')
+                    .replace(' )', ')')
                     .strip())
-        return True if new_pred == new_target else False
-
-    def _get_SP_query_results_from_db(self, df: pd.DataFrame, prediction_col_name: str
-                                      ) -> tuple[pd.DataFrame, str]:
-        """
-        Retrieve query results for the "prediction_col_name" column.
-        Since this is the prediction of SP model, this function returns None if the query is not valid.
-
-        Args:
-            df (pd.DataFrame): The input DataFrame containing query information.
-            prediction_col_name (str): The name of the column containing prediction values.
-
-        Returns:
-            tuple[pd.DataFrame, str]: A tuple containing the new DataFrame with query results and the column name for query results.
-        """
-
-        def wrapper_prediction(db_id, query):
-            """in case the prediction return an error, we return None"""
-            # to avoid multiple queries in the same string error
-            query = query.replace(';', '')
-            try:
-                output = self.databases.run_query(db_id, query)
-            except sqlite3.Error as e:
-                # catch any possible error of prediction and return None
-                logging.error(e)
-                return None
-            else:
-                return output
-
-        mask_equal = df.apply(
-            lambda row: self._create_mask_target_equal_prediction(row['query'],
-                                                                  row[prediction_col_name]),
-            axis=1
-        )
-        # create the new column for the prediction
-        new_prediction_col_name = f'{prediction_col_name}_result'
-        df.loc[mask_equal, new_prediction_col_name] = 'EQUAL'
-        # get prediction values for the elements not equal
-        tqdm.pandas(desc='Getting prediction results for not equal SQL predictions')
-        df.loc[~mask_equal, new_prediction_col_name] = df.loc[~mask_equal].progress_apply(
-            lambda row: wrapper_prediction(row['db_id'], row[prediction_col_name]),
-            axis=1
-        )
-        return df, new_prediction_col_name
+        return new_pred == new_target
